fc7c556e72199af738c56dbfa7ee24bcbdae84f5
macie <maciek.zok+github@gmail.com>
2016-09-30 18:28:40 +0200

    Add render engine

---
+++ smallstache.js
@@ -0,0 +1,10 @@
+function Smallstache(source) {
+    this.template = source.match(/^[#|\.].*/g) ?
+        document.querySelector(source) :  // source is selector
+        source;                           // source is template
+}
+
+Smallstache.prototype.render = function(obj) {
+    return this.template.replace(/{{\s*([\S|^}]+)\s*}}/g,
+        function(substring, matched) { return obj[matched]; });
+};
