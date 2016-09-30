f4086f9dd9381da2179adc2254b03b81d2e4fbb7
macie <maciek.zok+github@gmail.com>
2016-09-30 21:13:17 +0200

    Fix error with module export

--- Smallstache.js
+++ Smallstache.js
@@ -1,10 +1,12 @@
 function Smallstache(source) {
     this.template = source.match(/^[#|\.].*/g) ?
-        document.querySelector(source) :  // source is selector
+        window.document.querySelector(source) :  // source is selector
         source;                           // source is template
 }
 
 Smallstache.prototype.render = function(obj) {
     return this.template.replace(/{{\s*([\S|^}]+)\s*}}/g,
-        function(substring, matched) { return obj[matched]; });
+        function(substring, matched) { return obj[matched] });
 };
+
+module.exports = Smallstache;
